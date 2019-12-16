4cd0b03cb6b67212911a10abda5ce422d79a2546
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-16 22:34:35 +0800

    Create histogram.py

---
+++ histogram.py
@@ -0,0 +1,13 @@
+'''
+*********description*********
+this Class implement the abstract data structure "histogram", which make use of "HashMap" hash table for store 
+and query statistical related information.
+'''
+
+from hashmap import HashMap
+
+class Histogram:
+
+	# "catSeq" represent a particular sequence of category identifier.
+	def __init__(self, catSeq)
+	
