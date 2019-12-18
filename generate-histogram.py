ebedfb5da4d0b4dfab57802a6142591a16f842c0
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-18 11:15:28 +0800

    Update generate-histogram.py

--- generate-histogram.py
+++ generate-histogram.py
@@ -5,4 +5,49 @@ read a collection of data from a text file,
 and generate a statistics chart(occurrence frequency)  for it.
 '''
 
+from histogram import Histogram
+
+def scoreToLetterGrade(score):
+	
+	if score <=:
+		return 
+	elif score <=:
+		
+	elif score <=:
+		
+	elif score <=:
+		
+	
+def gen_chart():
+	
+	print(				"distribution")
+	
+	grade = ("A", "B", "C", "D", "E", "F")
+	for letter in grade:
+		count = instanceHistogram.getCount(letter)
+		print(letter + )
+		print("*" * count)
+	
+	
+	print(				"+")
+	print(				"------------------")
+	print(				"")
+
+	
+def main():
+	
+	instanceHistogram = Histogram("ABCDEF")
+	dataset = open("score_transcript.txt", r)
+	
+	for each_line in dataset:
+		
+		letter_grade = scoreToLetterGrade(int(score))
+		instanceHistogram.increaseCount(letter_grade)
+	
+	gen_chart(instanceHistogram)	
+	
+  
+main()
+
+
 
