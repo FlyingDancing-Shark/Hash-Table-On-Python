ad4d05484326f99b09691ccdaaad49431f35db1e
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-18 11:56:53 +0800

    Update generate-histogram.py

--- generate-histogram.py
+++ generate-histogram.py
@@ -7,6 +7,7 @@ and generate a statistics chart(occurrence frequency)  for it.
 
 from histogram import Histogram
 
+# receive a two-digits-based integer "score" as parameter
 def scoreToLetterGrade(score):
 	
 	if score >= 90 :
@@ -23,13 +24,21 @@ def scoreToLetterGrade(score):
 	
 def gen_chart(instanceHist):
 	
+	# indent to arrange this message right in the middle of the chart top
 	print("			Grade Distribution")
 	
+	# a set of hash keys use for query operation
 	grades = ('A', 'B', 'C', 'D', 'E', 'F')
 	for letter in grades:
 		print("    |")
 		print(letter + " +", end = "")
+		
+		# here, individual "key" act as an index into hash table to get corresponding "value" 
+		# (count for that "key" category)
 		count = instanceHist.getCount(letter)
+		
+		# multiply single asterisk by number we get multiple asterisk that visually represent 
+		# the appear frequency of a specific letter grade category 
 		print("*" * count)
 	
 	print("    |")
@@ -43,13 +52,13 @@ def main():
 	dataset = open('score_transcript.txt', "r")
 	
 	for each_line in dataset:
-		
 		letter_grade = scoreToLetterGrade(int(each_line))
 		instanceHistogram.increaseCount(letter_grade)
-	
+		
+	# when for loop exit, the five category have finished their statistic, ready for subsequently display
 	gen_chart(instanceHistogram)	
 
 
+# drive the main working process
 main()
-
 
