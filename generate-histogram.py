c5cd28d53e4e623ce71c9ed51a5d44cbff3d12fa
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-18 10:48:35 +0800

    Create generate-histogram.py

---
+++ generate-histogram.py
@@ -0,0 +1,8 @@
+'''
+*********description*********
+this main program make use of histogram class and underlying hash table implementation, 
+read a collection of data from a text file, 
+and generate a statistics chart(occurrence frequency)  for it.
+'''
+
+
