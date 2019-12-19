2ab6ff26cc42626bcc1c7f59b1b68ec7659a0a15
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-19 23:36:22 +0800

    Update sine_approximation.py

--- sine_approximation.py
+++ sine_approximation.py
@@ -5,19 +5,20 @@
 
 import math
 
-max_power = 7
-counter = 1
 x_for_sine = 7
-sign = 1.0
+max_power = 30
+counter = 1
 sum = x_for_sine
+sign = 1.0
 
-while counter < 7 :
+while counter < max_power :
 	
 	sign = -sign
 	counter += 2
 	trem = sign*x_for_sine**counter / math.factorial(counter)
 	sum += trem
 
-print "sin(%g) = %g (approximation with %d terms)" % (x_for_sine, sum, max_power) 	
+# this print syntax only legitimate for Python 2.X ,for Python 3.x , using print()	
+print 'sin(%g) = %.32f (approximation with %d terms)' % (x_for_sine, sum, max_power) 
   
 
