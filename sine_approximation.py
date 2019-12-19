504320ab869521c9323185afe517eae9dc9ae2d6
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-19 22:51:47 +0800

    Update sine_approximation.py

--- sine_approximation.py
+++ sine_approximation.py
@@ -5,10 +5,19 @@
 
 import math
 
-max_power = 5
-counter = 0
+max_power = 7
+counter = 1
 x_for_sine = 7
-sign = -1.0
+sign = 1.0
+sum = x_for_sine
 
-wihie(counter < 5):
+wihie counter < 7 :
+	
+	sign = -sign
+	counter += 2
+	trem = sign*x_for_sine**counter / math.factorial(counter)
+	sum += trem
+
+print 'sin(%g) = %g (approximation with %d terms)' % (x_for_sine, sum, max_power) 	
+  
 
