a63a0b61958641efd90857d395ad8fb2aff0b7b3
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-25 10:53:59 +0800

    Create count_each_letter_in_file.py

---
+++ count_each_letter_in_file.py
@@ -0,0 +1,21 @@
+'''
+*********description*********
+this program actually count the number of occurrences of each letter in a text file.
+'''
+
+from array import Array
+
+chrCounter = Array(127)
+chrCounter.clear(0)
+
+txtFile = open('my_arrays.txt', 'r')
+for line in txtFile:
+	for letter in line:
+    	ascii_code = ord(letter)
+    	chrCounter[ascii_code] += 1
+		
+txtFile.close()
+
+for i in range(26):
+	print( "%c - %4d                %c - %4d" % \
+			(chr(65+i), chrCounter[65+i], chr(97+i), chrCounter[97+i]) )
