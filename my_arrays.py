e2892ea3adaa7371ec6b7378d67c1646a3eb6a8c
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-25 10:47:24 +0800

    Update my_arrays.py

--- my_arrays.py
+++ my_arrays.py
@@ -1,7 +1,7 @@
 '''
 *********description*********
 note that we distinguish  this module from the Python build-in "array" module 
-by putting an "s" suffix on its name.
+by putting a "my_" sperfix and an "s" suffix on its name.
 '''
 
 import ctypes
