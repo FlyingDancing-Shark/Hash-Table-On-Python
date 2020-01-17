ef6f3f0e6055c319085f042b9a7ab97deb40211a
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-17 11:18:32 +0800

    Create helper_functions_collection.py

---
+++ helper_functions_collection.py
@@ -0,0 +1,18 @@
+'''
+*********description*********
+
+'''
+
+color_dict = {'red': ['5'], 'green': [''], 'blue': ['0']}
+
+
+def dictionary_helper(dict_obj, dict_key, default=0):
+    found_value = dict_obj.get(dict_key)
+    if found_value[0]:
+            result = int(found_value[0])
+    else:
+            result = default
+    return result
+
+value = dictionary_helper(color_dict, 'red')
+
