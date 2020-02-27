1894599247ceed6a6b372fa8674039a3ea60631e
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-02-27 09:15:47 +0800

    Update helper_functions_collection.py

--- helper_functions_collection.py
+++ helper_functions_collection.py
@@ -3,6 +3,9 @@
 
 '''
 
+from collections import defaultdict, Counter
+from pprint import pprint
+
 color_dict = {'red': ['5'], 'green': [''], 'blue': ['0']}
 
 
@@ -16,5 +19,17 @@ def dictionary_helper(dict_obj, dict_key, default=0):
 
 value = dictionary_helper(color_dict, 'red')
 
+##############################################################################
 
+def display_words_count(filename, first_n_word):
+    word_counts = defaultdict(int)
+    lines = [ line.strip('\n') for line in open(filename, 'r') ]
+    for line in lines:
+        for word in line.split(' '):
+            word_counts[word.lower()] += 1
+    words_freq = Counter(word_counts)
+    if '' in words_freq.most_common(first_n_word)[0]:
+        pprint(words_freq.most_common(first_n_word)[1:])
+        
+####################################################################################### 
 
