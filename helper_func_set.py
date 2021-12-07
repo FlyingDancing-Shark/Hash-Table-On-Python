36f174e4d077e82037323987ab42ed1c747f1c20
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-12-07 16:53:37 +0800

    Update helper_func_set.py

--- helper_func_set.py
+++ helper_func_set.py
@@ -90,10 +90,26 @@ worker: Worker = Worker()
     
 def create_hot_dog():
     bun = dispense_bun()
+    if bun is None:
+        print_error_code("Bun unavailable. Check for bun")
+        return
+    
     frank = dispense_frank()
+    if frank is None:
+        print_error_code("Frank was not properly dispensed")
+        return
+    
     hot_dog = bun.add_frank(frank)
+    if hot_dog is None:
+        print_error_code("Hot Dog unavailable. Check for Hot Dog")
+        return
+    
     ketchup = dispense_ketchup()
     mustard = dispense_mustard()
+    if ketchup is None or mustard is None:
+        print_error_code("Check for invalid catsup")
+        return
+    
     hot_dog.add_condiments(ketchup, mustard)
     dispense_hot_dog_to_customer(hot_dog)
 
