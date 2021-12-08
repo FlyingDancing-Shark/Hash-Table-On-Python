bf20a742aa0cd729ebe388389d60f15e3de33c3f
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-12-08 15:59:29 +0800

    Update helper_func_set.py

--- helper_func_set.py
+++ helper_func_set.py
@@ -100,6 +100,14 @@ def dispense_snack(user_input: str) -> Union[HotDog, Pretzel]:
     
     
     
-    
+from typing import Union
+def place_order() -> Optional[HotDog]:
+    order = get_order()
+    result = dispense_snack(order.name)
+    if result is None
+        print_error_code("An error occurred" + result)
+        return None
+ # Return our HotDog
+    return result    
     
 
