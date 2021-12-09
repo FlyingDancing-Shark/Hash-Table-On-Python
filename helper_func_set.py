c2c98ea78e793e5a0033c0d938d33a340f7c2266
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2021-12-09 13:12:03 +0800

    Update helper_func_set.py

--- helper_func_set.py
+++ helper_func_set.py
@@ -121,13 +121,13 @@ from typing import Union
 class Error:
     error_code: int
     disposed_of: bool
+        
 @dataclass
 class Snack:
     name: str
     condiments: set[str]
-    snack: Union[Snack, Error] = Snack("Hotdog", {"Mustard", "Ketchup"})
         
-        
+snack: Union[Snack, Error] = Snack("Hotdog", {"Mustard", "Ketchup"})
 snack = Error(5, True)
 
 
